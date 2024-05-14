4d0c96adb0af0beb2b52d68f53c5adaa6423e4f9
adam urbanowicz <adam.urbanow@gmail.com>
2024-05-14 12:52:28 +0200

    kolo

---
+++ select.sql
@@ -0,0 +1,17 @@
+#1. Liczy liczbę osób, które mają zarówno ojca, jak i matkę w bazie danych.
+
+
+
+#2. Wyświetla osoby, które mają więcej niż jedno wydarzenie życiowe, sortując je malejąco według liczby wydarzeń.
+
+
+
+#3. Wyświetla imiona i nazwiska osób, które zmarły po 2000 roku, wraz z datą ich śmierci.
+
+
+
+#4. Wyświetla osoby wraz z imionami i nazwiskami ich rodziców, jeśli są dostępni.
+
+
+
+#5. Wyświetla osoby, które mają wydarzenie 'Narodziny' lub 'Śmierć', łącząc wyniki z dwóch osobnych zapytań.
